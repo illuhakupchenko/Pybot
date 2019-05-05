2d85f879256f4132e5ee827dd666faae5009897b
Illya <36278563+illuhakupchenko@users.noreply.github.com>
2019-05-05 19:53:56 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,8 +7,8 @@ bot = telebot.TeleBot("801359509:AAHjuBl_1xRdDHHTTacpT3Q1TSiXl_qQiCw")
 def send_welcome(message1):
     bot.send_message(message1.chat.id, '<strong>Приветствую!</strong>\N{Victory Hand}\nЯ первый бот <a href="https://illuhakupchenko.github.io">'
 									   'Ильи Серегеевича</a>, меня зовут Botty3000\N{Clown Face}\n'
-                                       'На данный момент мой фунционал\N{Gear} не очень велик, но мой создатель '
-                                       'пообещал меня совершенствовать!\N{Game Die}', parse_mode="HTML")
+                                       'На данный момент мой функционал\N{Gear} не очень велик, но создатель '
+                                       'обещал меня совершенствовать в дальнейшем!\N{Game Die}', parse_mode="HTML")
 
 @bot.message_handler(content_types=['text'])
 def send_echo(message):
@@ -19,7 +19,7 @@ def send_echo(message):
     else:
         msg = 'nitonito'
 
-    bot.send_message(message.chat.id, '<a href="https://drive.google.com/open?id=19_EeOIOKIlBpIf8h-n3-qoXaOb-3ZxS6">hello</a>', parse_mode="HTML")
+    bot.send_message(message.chat.id, msg)
 
 
 bot.polling(none_stop=True)
