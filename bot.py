41d6a629e7791933f3d9d8073a7e8bce52a37411
Illya <36278563+illuhakupchenko@users.noreply.github.com>
2019-05-06 01:51:24 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,26 +7,28 @@ keyboard1.row('Привет', 'Пока')
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message1):
+    markup = telebot.types.InlineKeyboardMarkup()
+    button = telebot.types.InlineKeyboardButton(text="Сайт Ильи Сергеевича", url="https://illuhakupchenko.github.io")
+    markup.add(button)
     bot.send_message(message1.chat.id,
-                     '<strong>Приветствую!</strong>\N{Victory Hand}\nЯ первый бот <a href="https://illuhakupchenko.github.io">'
+                     '<strong>Приветствую!</strong>\N{Victory Hand}\nЯ первый бот <a href="">'
                      'Ильи Серегеевича</a>, меня зовут <i>Botty3000</i>\N{Robot Face}\n'
                      'На данный момент мой функционал\N{Gear} не очень велик, но создатель '
                      'обещал меня совершенствовать!\N{Flexed Biceps}\n'
                      'Чем я могу вам помочь? Выберите категорию\N{Game Die}', parse_mode="HTML", reply_markup=keyboard1)
-    bot.send_message(message1.chat.id, 'kek000')
-
+    bot.send_photo(message1.chat.id, photo='https://static-ck.okdesk.ru/okdesksite/pictures/115/content_bot.jpg', reply_markup=markup)
 
 @bot.message_handler(content_types=['text'])
 def send_echo(message):
     if message.text.lower() == 'kek':  # lower() - ф-ция для отбора верхнего и нижнего регистров
         msg = 'dro4'
+        bot.send_sticker(message.chat.id, 'CAADAgAD0gEAAiUDUg-5ImyW6RwdWAI')
     elif message.text.lower() == 'lol':
         msg = 'kekun'
     else:
         msg = 'nitonito'
 
     bot.send_message(message.chat.id, msg)
-    bot.send_sticker(message.chat.id, 'CAADAgAD0gEAAiUDUg-5ImyW6RwdWAI')
 
 
 bot.polling(none_stop=True)
